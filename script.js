5d04153ec1af418c3301f83e344665385fd1a515
annruiblaz <anna.ruiz.dev@gmail.com>
2025-03-03 20:22:17 +0100

    takePhoto + upload to server working

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ const VIDEO_ELEMENT = document.getElementById('video');
 const IMGS_CONTAINER = document.getElementById('imgs-container');
 const FILES_INPUT = document.getElementById('files-input');
 const INTERVAL_INPUT = document.getElementById('interval');
+const NUM_PHOTOS_INPUT = document.getElementById('numPhotos');
 const PHOTO_BTN = document.getElementById('photo-btn');
 const UPLOAD_BTN = document.getElementById('upload-btn');
 
-let isCapturing = false;
+//let isCapturing = false;
 let capturedImgs = [];
 let uploadedImgs = [];
 let classNames = [];
+let numPhotos = 0;
+let contador = 0;
 
 async function getAccessWebcam() {
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-/*         const stream = await navigator.mediaDevices.getUserMedia({video: true});
-        VIDEO_ELEMENT.srcObject = stream; */
+        /*         const stream = await navigator.mediaDevices.getUserMedia({video: true});
+                VIDEO_ELEMENT.srcObject = stream; */
         navigator.mediaDevices.getUserMedia({
                 audio: false,
                 video: {
@@ -21,9 +24,10 @@ async function getAccessWebcam() {
                     height: 375
                 }
             })
-            .then( function (stream) {
+            .then(function (stream) {
                 VIDEO_ELEMENT.srcObject = stream;
-            }).catch( function (error) {
+                isCapturing = true;
+            }).catch(function (error) {
                 console.error('Se ha producido un error al acceder a la webcam. :(');
             });
     } else {
@@ -31,26 +35,93 @@ async function getAccessWebcam() {
     }
 }
 
-function uploadImgs() {
-    let nameClass = document.getElementById('class-1').textContent;
-    console.log(nameClass);
+FILES_INPUT.addEventListener('change', (event) => {
+    //Obtenemos el nombre d la clase introducido x el user
+    let nameClass = document.getElementById('class-1').value;
+
+    //Obtenemos las imgs seleccionadas del user
+    const files = event.target.files;
+
+    //Iteramos sobre cada 1 d los archivos
+    for (let file of files) {
+        //creamos el lector d archivos
+        const reader = new FileReader();
+
+        //Una vez el lector tiene el archivo cargado
+        reader.onload = (event) => {
+            //Almacenamos la img en el array
+            capturedImgs.push(event.target.result);
+
+            //Creamos un elemento <img> para mostrarla
+            const img = document.createElement('img');
+            img.src = event.target.result;
+            //La añadimos en la clase q corresponde
+            IMGS_CONTAINER.appendChild(img);
+        };
+
+        //Leemos el archivo como una url d datos en base64
+        reader.readAsDataURL(file);
+    }
+});
+
+UPLOAD_BTN.addEventListener('click', async () => {
+    let nameClass = document.getElementById('class-1').value;
+
+    //Enviamos los datos en un obj FormData (para + facilidad)
+    const formData = new FormData();
+
+    //Convierte cada img almacenada a un blob
+    for (let i = 0; i < capturedImgs.length; i++) {
+        //convertimos con blob en una img base64
+        const blob = await (await fetch(capturedImgs[i])).blob();
+
+        let nameImg = `${nameClass}${i}.png`;
+        console.log(nameImg);
+
+        //se agrega la img al formData con su nombre
+        formData.append('images', blob, nameImg);
+    }
+
+    console.log('formData tras fori: ', formData);
+
+    try {
+        //se envian con una petición POST las imgs al server
+        const response = await fetch('http://localhost:3000/imagenes', {
+            method: 'POST',
+            body: formData
+        });
+
+        //convierte la respuesta a json y la printeamos
+        const result = await response.json();
+        console.log('Imágenes subidas: ', result);
+
+    } catch(err) {
+        console.error('Error al subir las imágenes: ', err);
+    }
+});
+
+
+// TODO --> Optimizar esta guarrada (ROTO)
+
+function initTakePhotos() {
+    let interval = INTERVAL_INPUT.valueAsNumber;
+    let numPhotos = NUM_PHOTOS_INPUT.valueAsNumber;
+
+    console.log(`Interval: ${interval}, NumPhotos: ${numPhotos}`);
+
+    function takeNextPhoto() {
+        //Para q espere bien todo el setTimeout necesitamos utilizarlo d manera recursivaa!!!
+        if (contador < numPhotos) {
+            contador += 1;
+            takeImg(); //tomamos y mostramos la captura
+            setTimeout(recursivePhoto, interval); //Volvemos a llamar a la funcion una vez pasa el intervalo d ms
+        }
+    }
+
+    takeNextPhoto();
 }
 
-PHOTO_BTN.addEventListener('mousedown', () => {
-    isCapturing = true;
-    takePhotos();
-});
-
-PHOTO_BTN.addEventListener('mouseup', () => {
-    isCapturing = false;
-});
-
-function takePhotos() {
-    if(!isCapturing) return;
-    
-    let interval = INTERVAL_INPUT.value + '000';
-    console.log(parseInt(interval));
-
+function takeImg() {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
 
@@ -60,14 +131,10 @@ function takePhotos() {
     context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     const dataURL = canvas.toDataURL('image/png');
-    capturedImgs.push(dataURL);
+    capturedImgs.push(dataURL); //añadimos a array d imgs capturadas 
 
-    //Mostramos las imgs capturadas en el html
     const img = document.createElement('img');
     img.src = dataURL;
     IMGS_CONTAINER.appendChild(img);
-
-    setTimeout(takePhotos, parseInt(interval));
-
+    console.log('Contador en takeImg', contador);
 }
-
--- server/server.js
+++ server/server.js
@@ -5,22 +5,35 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.static('/server/uploads'));
 
+
+app.use(express.static('/server/imagenes'));
+
+//app.use('/imagenes', express.static(path.join(__dirname,'imagenes')));
+
+console.log(path.join(__dirname,'imagenes'));
+
+//config d almacenamiento d archivos con multer
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/')
+        cb(null, 'imagenes/');
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
+        cb(null, Date.now() + '_' + file.originalname);
     }
 });
 
 const upload = multer({storage});
 
-app.post('/upload', upload.array('images', 10), (req, res) => {
+//Ruta para subir varias imgs 
+                            // TODO: Retirar el 10 d num max
+app.post('/imagenes', upload.array('images', 10), (req, res) => {
+    //extraemos a un array solo el valor del name
     const files = req.files.map(file => file.filename);
-    res.json({message: 'Imágenes subidas con éxito', filename});
+
+    //devolvemos la respuesta d todo ok con el array d nombres d archivos subidos
+    res.json({message: 'Imágenes subidas con éxito', files});
 });
 
+//inicia el server
 app.listen(3000, () => console.log('Servidor activo en http://localhost:3000'));
